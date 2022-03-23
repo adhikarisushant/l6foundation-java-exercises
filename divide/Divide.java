69d19f83bce222483d56dedcc0fad172ac4c6648
adhikarisushant <sushant.adhikari.619@gmail.com>
2022-03-23 21:59:00 +0545

    first commit

---
+++ divide/Divide.java
@@ -0,0 +1,24 @@
+public class Divide {
+    public static void main(String args[]) {
+        int num1=50, num2=3;
+        int result = num1/num2;
+        System.out.println("Division of"+num1+"and"+num2+"is:"+result);
+    }
+}
+
+
+// import java.util.Scanner;
+
+// public class Divide {
+//   public static void main(String[] args) 
+//   {
+//     Scanner input = new Scanner (System.in);
+//     System.out.print("Input the first number: ");
+//     int a = input.nextInt();
+//     System.out.print("Input the second number: ");
+//     int b = input.nextInt();
+//     int d = (a/b);
+//     System.out.println();
+//     System.out.println("The division of a and b is:" +d);
+//   }
+// }
---
+++ exercise-0/Main.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Other/File.java to edit this template
+ */
+
+/**
+ *
+ * @author sushant
+ */
+
+
+ import java.util.Scanner;
+
+public class Main {
+  public static void main(String[] args) 
+  {
+    Scanner input = new Scanner (System.in);
+    System.out.print("Input the first number: ");
+    int a = input.nextInt();
+    System.out.print("Input the second number: ");
+    int b = input.nextInt();
+    int d = (a/b);
+    System.out.println();
+    System.out.println("The division of a and b is:" +d);
+  }
+}
